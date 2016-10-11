d1523b0460d2101709ce62a0f711f11118bc509a
Qx <790303684@qq.com>
2016-10-11 11:21:38 +0800

    Initial commit

---
+++ app/src/main/java/qx/com/swiperefreshlayoutdemo/MyItemDec.java
@@ -0,0 +1,36 @@
+package qx.com.swiperefreshlayoutdemo;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+public class MyItemDec extends RecyclerView.ItemDecoration {
+
+
+    /*
+    渲染item前调用此方法
+     */
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+    }
+    /*
+       渲染item后调用此方法
+        */
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+    }
+
+    /*
+    设置item的边距
+     */
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+    }
+}
